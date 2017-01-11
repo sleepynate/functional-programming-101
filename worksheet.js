0be35779e6a0dec195877bf25e5798b0620688f0
nathan dotz <nathan.dotz@gmail.com>
2017-01-11 17:59:07 -0500

    Oops, don't leave the answers in the worksheet

--- worksheet.js
+++ worksheet.js
@@ -22,22 +22,3 @@ const cons = (x, xs) => {
     ys.unshift(x);
     return ys;
 };
-
-class Node {
-  constructor(value, left, right) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
-  }
-
-  add(value) {
-    if (value < this.value) {
-      if (this.left) return new Node(this.value, this.left.add(value), this.right);
-      else return new Node(this.value, new Node(value), this.right);
-    } else {
-      if (this.right) return new Node(this.value, this.left, this.right.add(value));
-      else return new Node(this.value, this.left, new Node(value));
-    }
-  }
-
-}
